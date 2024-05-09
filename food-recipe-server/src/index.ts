9304432712bc747c50fcbd905658d7d43a46bc60
Ritesh jha <jhar27018@gmail.com>
2024-05-09 15:09:31 +0530

    Data modelling is completed

--- food-recipe-server/src/index.ts
+++ food-recipe-server/src/index.ts
@@ -3,6 +3,7 @@ import { logger } from "hono/logger";
 import { cors } from "hono/cors";
 import { csrf } from "hono/csrf";
 import { prettyJSON } from "hono/pretty-json";
+import { v2 as cloudinary } from "cloudinary";
 const app = new Hono();
 
 // middlewares
@@ -10,5 +11,12 @@ app.use(logger());
 app.use(cors({ origin: Bun.env.ORIGIN! }));
 app.use(csrf({ origin: Bun.env.ORIGIN! }));
 app.use(prettyJSON());
+
+// Cloudinary configuration
+cloudinary.config({
+  cloud_name: Bun.env.CLOUDINARY_CLOUD_NAME,
+  api_key: Bun.env.CLOUDINARY_API_KEY,
+  api_secret: Bun.env.CLOUDINARY_API_SECRET,
+});
 
 export default app;
