8f21bc5d6f7b2dc00f989f28614bac2681b0a9b1
Ritesh jha <jhar27018@gmail.com>
2024-05-11 11:27:42 +0530

    frontend setup with tamagui

---
+++ food-recipe-client/app/_layout.tsx
@@ -0,0 +1,41 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import "@tamagui/core/reset.css";
+import { TamaguiProvider } from "tamagui";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import tamaguiConfig from "@/tamagui.config";
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+  return (
+    <TamaguiProvider config={tamaguiConfig}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </ThemeProvider>
+    </TamaguiProvider>
+  );
+}
